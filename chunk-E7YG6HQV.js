4d5a67ad4794d55a7635680e2c6664bcad768fdb
Samy801172 <abdeljalil.lemkadem@student.efpl.be>
2025-05-22 23:21:50 +0200

    Auto-generated commit

---
+++ chunk-E7YG6HQV.js
@@ -0,0 +1,1 @@
+var t={production:!1,apiUrl:"http://localhost:2024/api",stripe:{publishableKey:"pk_test_51R5qVlCKECr1cYK1BbpbQwLBLDwMyxSGlk1A6EWGuPgEBQk0OKJQu9h0kECev2N3ZbYPI483cPajUkjD5l6DkjMW00CEohJsBq"}};export{t as a};
