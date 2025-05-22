e7f800a82ccbb95e957da2e8241fef280ac6fd87
Samy801172 <163838134+Samy801172@users.noreply.github.com>
2025-05-22 19:39:55 +0200

    Delete my-app directory

--- my-app/src/app/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './user.service';
-export * from './wallet.service';
-export * from './subscription.service';
-export * from './transaction.service';
-export * from './subscription.service';
-export * from './forum.service';
-export * from './cryptocurrency.service';
